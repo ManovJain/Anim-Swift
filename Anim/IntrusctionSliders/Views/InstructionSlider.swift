4178bd245ffafb267eec466c339f5885245d58fe
brian.pattison <brian.pattison@cognizant.com>
2023-01-23 14:16:57 -0600

    Adding slideshow framework

---
+++ Anim/IntrusctionSliders/Views/InstructionSlider.swift
@@ -0,0 +1,42 @@
+//
+//  InstructionSlider.swift
+//  Anim
+//
+//  Created by Pattison, Brian (Cognizant) on 1/23/23.
+//
+
+import SwiftUI
+
+struct InstructionSlider: View {
+    
+    let defaults = UserDefaults.standard
+    
+    @State var index = 0
+    
+    @Binding var openedApp: Bool
+
+    var images = ["10-12", "10-13", "10-14", "10-15"]
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            PagingView(index: $index.animation(), maxIndex: images.count - 1) {
+                ForEach(self.images, id: \.self) { imageName in
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            Button {
+                defaults.set(true, forKey: "openedApp")
+                openedApp = true
+            }
+        label: {
+            Text("Dismiss")
+        }
+            //            .aspectRatio(contentMode: .fill)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        }
+        .padding()
+        
+    }
+}
