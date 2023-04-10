50c103d23944a0fccd79b987dec79def7e426c61
Manov Jain <manovjainllc@gmail.com>
2023-04-10 09:11:42 -0700

    cleaned up fav button

--- Anim/ScannedFoodPage/Views/New Product Display/SpinningStarButton.swift
+++ Anim/ScannedFoodPage/Views/New Product Display/SpinningStarButton.swift
@@ -25,38 +25,25 @@ struct SpinningStarButton: View {
                 
                 userViewModel.user.favorites = userViewModel.user.favorites?.filter { $0 != (id) }
                 
-                withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.5)) {
-                        self.isTapped.toggle()
-                    }
-                    
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-//                        withAnimation(.spring()) {
-//                            self.isTapped = false
-//                        }
-//                    }
+                withAnimation(.spring()) {
+                                self.isTapped.toggle()
+                            }
             }) {
                 Image(systemName: "heart.fill")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: isTapped ? 50 : 30, height: isTapped ? 50 : 30)
                     .foregroundColor(isTapped ? .gray : .pink)
-                    .rotationEffect(.degrees(isTapped ? 360 : 0))
-                    .animation(.spring())
+                    .rotationEffect(.degrees(isTapped ? 0 : 360))
             }
         } else {
             Button(action: {
                 
                 userViewModel.user.favorites?.append(id)
                 
-                withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.5)) {
-                        self.isTapped.toggle()
-                    }
-                    
-//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-//                        withAnimation(.spring()) {
-//                            self.isTapped = false
-//                        }
-//                    }
+                withAnimation(.spring()) {
+                                self.isTapped.toggle()
+                            }
                 
             }) {
                 Image(systemName: "heart.fill")
@@ -65,7 +52,6 @@ struct SpinningStarButton: View {
                     .frame(width: isTapped ? 50 : 30, height: isTapped ? 50 : 30)
                     .foregroundColor(isTapped ? .pink : .gray)
                     .rotationEffect(.degrees(isTapped ? 360 : 0))
-                    .animation(.spring())
             }
         }
     }
