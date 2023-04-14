cf846c57f492e7bfbff6af74e31b5cefccf0f99a
brian.pattison <brian.pattison@cognizant.com>
2023-04-14 10:14:11 -0500

    scroll view fix

--- Anim/Social/Views/CreatePostView.swift
+++ Anim/Social/Views/CreatePostView.swift
@@ -17,50 +17,52 @@ struct CreatePostView: View {
     @State var togglePreview = false
     
     var body: some View {
-        VStack {
-            if verifyUrl(urlString: link) {
-                Spacer()
-                    .frame(height: 25)
-                URLPreview(previewURL: URL(string: link)!, togglePreview: self.$togglePreview)
-                    .fixedSize()
-                    .frame(height: 200)
-                    .padding()
-            }
-            else {
-                Spacer()
-            }
-            Spacer()
-                .frame(height: 50)
-            if (!(verifyUrl(urlString: link) ) && !link.isEmpty) {
-                Text("Invalid URL")
-                    .foregroundColor(.red)
-                    .font(Font.custom("DMSans-Medium", size: 12))
-            }
-            TextField(
-                "Recipe Link",
-                text: $link
-            )
-            .font(Font.custom("DMSans-Medium", size: 16))
-            .padding()
-            .border(Color("AnimGreen"), width: 1)
-            TextField(
-                "Caption",
-                text: $caption
-            )
-            .font(Font.custom("DMSans-Medium", size: 16))
-            .padding()
-            .border(Color("AnimGreen"), width: 1)
-            Button(action: {
-                FirestoreRequests().createPost(userID: userViewModel.user.uid!, postID: UUID().uuidString, contentType: "link", content: link, caption: caption, datePosted: Date(), username: userViewModel.user.username!, isPublic: userViewModel.user.isPublic!) { data in
-                    NotificationCenter.default.post(name: NSNotification.refreshPosts, object: nil)
-                    self.presentationMode.wrappedValue.dismiss()
+        ScrollView {
+            VStack {
+                if verifyUrl(urlString: link) {
+                    Spacer()
+                        .frame(height: 25)
+                    URLPreview(previewURL: URL(string: link)!, togglePreview: self.$togglePreview)
+                        .fixedSize()
+                        .frame(height: 200)
+                        .padding()
                 }
-            })
-            {
-                Text("Post")
+                else {
+                    Spacer()
+                }
+                Spacer()
+                    .frame(height: 50)
+                if (!(verifyUrl(urlString: link) ) && !link.isEmpty) {
+                    Text("Invalid URL")
+                        .foregroundColor(.red)
+                        .font(Font.custom("DMSans-Medium", size: 12))
+                }
+                TextField(
+                    "Recipe Link",
+                    text: $link
+                )
+                .font(Font.custom("DMSans-Medium", size: 16))
+                .padding()
+                .border(Color("AnimGreen"), width: 1)
+                TextField(
+                    "Caption",
+                    text: $caption
+                )
+                .font(Font.custom("DMSans-Medium", size: 16))
+                .padding()
+                .border(Color("AnimGreen"), width: 1)
+                Button(action: {
+                    FirestoreRequests().createPost(userID: userViewModel.user.uid!, postID: UUID().uuidString, contentType: "link", content: link, caption: caption, datePosted: Date(), username: userViewModel.user.username!, isPublic: userViewModel.user.isPublic!) { data in
+                        NotificationCenter.default.post(name: NSNotification.refreshPosts, object: nil)
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                })
+                {
+                    Text("Post")
+                }
+                .disabled(!verifyUrl(urlString: link))
+                Spacer()
             }
-            .disabled(!verifyUrl(urlString: link))
-            Spacer()
         }
         .onDisappear() {
             NotificationCenter.default.post(name: NSNotification.refreshPost, object: nil)
